6f69f82fbf7bab7934284f7afa948d16c04c6f39
Florin <florinconstantin99@yahoo.com>
2023-10-03 23:51:24 +0300

    initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const navIcons = [
+  { src: "/assets/icons/search.svg", alt: "search" },
+  { src: "/assets/icons/black-heart.svg", alt: "heart" },
+  { src: "/assets/icons/user.svg", alt: "user" },
+];
+
+const Navbar = () => {
+  return (
+    <header className="w-full">
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/assets/icons/logo.svg"
+            width={27}
+            height={27}
+            alt="logo"
+          />
+
+          <p className="nav-logo">
+            Deal<span className="text-primary">Dive</span>
+          </p>
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
