f298a2cfe566c0ae7405d9615302a4095a2cc7fd
Florin <florinconstantin99@yahoo.com>
2023-10-05 02:02:47 +0300

    updated product card

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,17 +1,17 @@
-import { Product } from '@/types';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react'
+import { Product } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 interface Props {
   product: Product;
 }
- 
+
 const ProductCard = ({ product }: Props) => {
   return (
     <Link href={`/products/${product._id}`} className="product-card">
       <div className="product-card_img-container">
-        <Image 
+        <Image
           src={product.image}
           alt={product.title}
           width={200}
@@ -24,10 +24,6 @@ const ProductCard = ({ product }: Props) => {
         <h3 className="product-title">{product.title}</h3>
 
         <div className="flex justify-between">
-          <p className="text-black opacity-50 text-lg capitalize">
-            {product.category}
-          </p>
-
           <p className="text-black text-lg font-semibold">
             <span>{product?.currency}</span>
             <span>{product?.currentPrice}</span>
@@ -35,7 +31,7 @@ const ProductCard = ({ product }: Props) => {
         </div>
       </div>
     </Link>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
